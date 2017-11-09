463961b3216388a5dd611396e641705422f9d49a
Princess Luna <hisroyalhighnessprincessluna@gmail.com>
2017-11-09 01:14:51 +0100

    TheEssentialsSweet, ac les couleurs, sans les headers :3

---
+++ coreshit/Essentials/colors.h
@@ -0,0 +1,26 @@
+
+#ifndef COLORS_H
+# define COLORS_H
+
+# define DEFAULT "\x1B[0m"
+# define GREY "\x1B[38;5;252m"
+# define DARK "\x1B[38;5;246m"
+# define RED "\x1B[38;5;203m"
+# define GREEN "\x1B[38;5;119m"
+# define YELLOW "\x1B[38;5;227m"
+# define BLUE "\x1B[38;5;69m"
+# define MAGENTA "\x1B[38;5;207m"
+# define CYAN "\x1B[38;5;87m"
+# define WHITE "\x1B[38;5;231m"
+
+# define PINK "\033[38;5;219m"
+# define PEACHY "\033[38;5;215m"
+# define SKY "\033[38;5;117m"
+# define INDIGO "\033[38;5;99m"
+# define PURPLE "\033[38;5;141m"
+# define FOAM "\033[38;5;155m"
+# define SPRING "\033[38;5;112m"
+
+# define CAS( n ) "\033[38;5;" #n "m"
+
+#endif
