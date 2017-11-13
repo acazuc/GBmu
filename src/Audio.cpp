f7edb8fa981482ebd750946d4da521a600ae90e8
Jukino <acazuc@student.42.fr>
2017-11-13 12:34:47 +0100

    Added channel 3

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -16,6 +16,9 @@ static uint64_t c1env = 0;
 static uint64_t c2env = 0;
 static uint64_t c3env = 0;
 static uint64_t c4env = 0;
+static uint64_t c1freq = 0;
+static uint64_t c2freq = 0;
+static uint64_t c3freq = 0;
 static uint64_t c1nextenvstepchange = 0;
 static uint64_t c1nextswpstepchange = 0;
 static uint64_t c2nextenvstepchange = 0;
@@ -37,11 +40,11 @@ static uint8_t NR22 = 0b10011011;
 static uint8_t NR23 = 0b10101101;
 static uint8_t NR24 = 0b00100011;
 
-static uint8_t NR30 = 0b00000000;
+static uint8_t NR30 = 0b10000000;
 static uint8_t NR31 = 0b00000000;
-static uint8_t NR32 = 0b00000000;
-static uint8_t NR33 = 0b00000000;
-static uint8_t NR34 = 0b00000000;
+static uint8_t NR32 = 0b00100000;
+static uint8_t NR33 = 0b11011111;
+static uint8_t NR34 = 0b10000111;
 
 static uint8_t NR41 = 0b00000000;
 static uint8_t NR42 = 0b11110111;
@@ -49,18 +52,21 @@ static uint8_t NR43 = 0b01100000;
 static uint8_t NR44 = 0b10000000;
 
 static uint8_t NR50 = 0b01000100;
-static uint8_t NR51 = 0b10001000;
-static uint8_t NR52 = 0b10001000;
+static uint8_t NR51 = 0b01000100;
+static uint8_t NR52 = 0b10000100;
+
+
+static uint8_t mem[0xFFFF];
 
 static uint32_t c1regtofreq(uint8_t nr13, uint8_t nr14)
 {
 	uint16_t reg = (uint16_t)nr13 | (((uint16_t)(nr14 & 0x7)) << 8);
-	return (4194304 / (4 * 2 * (2048 - reg)));
+	return (4194304 / (32 * (2048 - reg)));
 }
 
 static uint16_t c1freqtoval(uint32_t freq)
 {
-	return (2048 - 4194304 / (4 * 2 * freq));
+	return (2048 - 4194304 / (32 * freq));
 }
 
 static void c1freqtoreg(uint32_t freq, uint8_t *nr13, uint8_t *nr14)
@@ -73,12 +79,12 @@ static void c1freqtoreg(uint32_t freq, uint8_t *nr13, uint8_t *nr14)
 static uint32_t c2regtofreq(uint8_t nr23, uint8_t nr24)
 {
 	uint16_t reg = (uint16_t)nr23 | (((uint16_t)(nr24 & 0x7)) << 8);
-	return (4194304 / (4 * 2 * (2048 - reg)));
+	return (4194304 / (32 * (2048 - reg)));
 }
 
 static uint16_t c2freqtoval(uint32_t freq)
 {
-	return (2048 - 4194304 / (4 * 2 * freq));
+	return (2048 - 4194304 / (32 * freq));
 }
 
 static void c2freqtoreg(uint32_t freq, uint8_t *nr23, uint8_t *nr24)
@@ -88,12 +94,19 @@ static void c2freqtoreg(uint32_t freq, uint8_t *nr23, uint8_t *nr24)
 	*nr24 = (*nr24 & 0b11111000) | (reg >> 8);
 }
 
+static uint32_t c3regtofreq(uint8_t nr33, uint8_t nr34)
+{
+	uint16_t reg = (uint16_t)nr33 | (((uint16_t)(nr34 & 0x7)) << 8);
+	return (4194304 / (32 * (2048 - reg)));
+}
+
 static int16_t getc1val()
 {
 	if (NR14 & 0b10000000)
 	{
 		c1len = (64 - (NR11 & 0b00111111)) * freq / 256;
 		c1env = (NR12 & 0b11110000) >> 4;
+		c1freq = c1regtofreq(NR13, NR14);
 		NR14 &= 0b01111111;
 	}
 	if (NR14 & 0b01000000)
@@ -108,7 +121,6 @@ static int16_t getc1val()
 			c1len--;
 		}
 	}
-	uint32_t c1freq = c1regtofreq(NR13, NR14);
 	if (frame >= c1nextenvstepchange)
 	{
 		uint8_t envstep = NR12 & 0b00000111;
@@ -154,7 +166,6 @@ static int16_t getc1val()
 			}
 		}
 	}
-	c1freqtoreg(c1freq, &NR13, &NR14);
 	uint8_t duty = (NR11 & 0b11000000) >> 6;
 	float dutyper = 0;
 	if (duty == 0b11)
@@ -166,7 +177,7 @@ static int16_t getc1val()
 	else if (duty == 0b00)
 		dutyper = .875;
 	uint32_t inter = freq / c1freq;
-	uint32_t curr = frame & inter;
+	uint32_t curr = frame % inter;
 	float envfac = c1env / 15.;
 	if (curr / (float)inter > dutyper)
 		return (SHRT_MAX * envfac);
@@ -182,6 +193,7 @@ static int16_t getc2val()
 		c2env = (NR22 & 0b11110000) >> 4;
 		NR24 &= 0b01111111;
 	}
+	c2freq = c2regtofreq(NR23, NR24);
 	if (NR24 & 0b01000000)
 	{
 		if (c2len == 0)
@@ -194,7 +206,6 @@ static int16_t getc2val()
 			c2len--;
 		}
 	}
-	uint32_t c2freq = c1regtofreq(NR23, NR24);
 	if (frame >= c1nextenvstepchange)
 	{
 		uint8_t envstep = NR22 & 0b00000111;
@@ -216,7 +227,6 @@ static int16_t getc2val()
 			}
 		}
 	}
-	c2freqtoreg(c2freq, &NR23, &NR24);
 	uint8_t duty = (NR21 & 0b11000000) >> 6;
 	float dutyper = 0;
 	if (duty == 0b11)
@@ -228,7 +238,7 @@ static int16_t getc2val()
 	else if (duty == 0b00)
 		dutyper = .875;
 	uint32_t inter = freq / c2freq;
-	uint32_t curr = frame & inter;
+	uint32_t curr = frame % inter;
 	float envfac = c2env / 15.;
 	if (curr / (float)inter > dutyper)
 		return (SHRT_MAX * envfac);
@@ -237,9 +247,15 @@ static int16_t getc2val()
 
 static int16_t getc3val()
 {
+	if (!(NR30 & 0b10000000))
+	{
+		NR52 &= 0b11111011;
+		return (0);
+	}
 	if (NR34 & 0b10000000)
 	{
 		c3len = (256 - NR31) * freq / 256;
+		c3freq = c3regtofreq(NR33, NR34);
 		NR34 &= 0b01111111;
 	}
 	if (NR34 & 0b01000000)
@@ -254,7 +270,29 @@ static int16_t getc3val()
 			c3len--;
 		}
 	}
-	return (0);
+	else
+	{
+		c3freq = c3regtofreq(NR33, NR34);
+	}
+	uint32_t inter = freq / c3freq;
+	if (!inter)
+		return (0);
+	uint8_t idx = (frame % inter) / (float)inter * 32;
+	uint8_t tmp = mem[0xFF30 + idx / 2];
+	if (!(idx & 1))
+		tmp = tmp >> 4;
+	tmp &= 0xF;
+	float fac = tmp / 16.;
+	int16_t val = -SHRT_MIN;
+	val += (SHRT_MAX - SHRT_MIN) * fac;
+	uint8_t level = (NR32 & 0b01100000) >> 5;
+	if (level == 0)
+		return (0);
+	if (level == 1)
+		return (val);
+	if (level == 2)
+		return (val / 2);
+	return (val / 4);
 }
 
 static int16_t getc4val()
@@ -400,6 +438,10 @@ static int paCallback(const void *input, void *output, unsigned long frameCount,
 
 Audio::Audio()
 {
+	*((uint32_t*)&(mem[0xFF30])) = 0x01234567;
+	*((uint32_t*)&(mem[0xFF34])) = 0x89ABCDEF;
+	*((uint32_t*)&(mem[0xFF38])) = 0xEDCBA987;
+	*((uint32_t*)&(mem[0xFFFC])) = 0x65432100; 
 	PaStreamParameters parameters;
 	parameters.device = Pa_GetDefaultOutputDevice();
 	parameters.channelCount = 2;
